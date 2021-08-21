74701b3530243cd76e4be0989e1c735d2ea28e4c
youareit125 <86081352+youareit125@users.noreply.github.com>
2021-08-21 13:25:21 -0500

    Add files via upload

--- Ar_CRM/Rate_Coefficients_Ar.m
+++ Ar_CRM/Rate_Coefficients_Ar.m
@@ -1,1 +1,20 @@
+clear
+clc
+close all
+
+[~,sheets] = xlsfinfo('Q_e_Ar.xlsx');
+
+data = [];
+data_curr = [];
+for i = 1:numel(sheets)
+    if i == 1
+        data = readtable('Q_e_Ar.xlsx', 'Sheet' ,i);
+    else
+        data_curr = readtable('Q_e_Ar.xlsx', 'Sheet' ,i);
+    end
+    data = [data; data_curr];
+end
+
+Te = linspace(0.1, 10^3, 10^4); %eV
+Tg = 5000; %Kelvin
 
